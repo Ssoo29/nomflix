737dd78cf1cd44d03042bea4873c5ab688e31281
김성수 <fereverv@openbase.co.kr>
2021-05-09 22:20:41 +0900

    API service(axios) 추가

---
+++ src/api.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: "https://www.themoviedb.org/",
+  params: {
+    api_key : "9db98926810069b9a51ddf3080645e7d",
+    language: "en-US"
+  }
+});
+
+export const movieApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upComing: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
+  movieDetail: id => api.get(`movie/${id}`, {
+    params: {
+      append_to_respone: "videos"
+    }
+  }),
+  search: term => api.get("search/movie", {
+    params: {
+      query: encodeURIComponent(term)
+    }
+  })
+}
+
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+  tvShowDetail: id => api.get(`tv/${id}`, {
+    params: {
+      append_to_respone: "videos"
+    }
+  }),
+  search: term => api.get("search/tv", {
+    params: {
+      query: encodeURIComponent(term)
+    }
+  })
+}
