efde03b69788a39d2ad661c97bb09b7878bbbddc
김성수 <fereverv@openbase.co.kr>
2021-05-10 22:11:42 +0900

    Container 구축

---
+++ src/Routes/Search/SearchContainer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import SearchPresenter from "./SearchPresenter";
+
+export default class Search extends React.Component {
+  state = {
+    movieResults: null,
+    tvShowResults: null,
+    searchTerm: "",
+    error: null,
+    loading: false
+  };
+
+ render (){
+  const { movieResults, tvShowResults, searchTerm, error, loading } = this.state;
+   return (<SearchPresenter 
+    movieResults = {movieResults}
+    tvShowResults = {tvShowResults}
+    searchTerm = {searchTerm}
+    error = {error}
+    loading = {loading}
+   />);
+ }
+}
