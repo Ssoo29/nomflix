b312283a5c859d5d37185edd4c1489caa7c7d395
김성수 <fereverv@openbase.co.kr>
2021-07-20 23:07:21 +0900

    header 수정

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
 
-const Header1 = styled.header`
+const HeaderNode = styled.header`
   color: white;
   position: fixed;
   top: 0;
@@ -12,42 +12,43 @@ const Header1 = styled.header`
   display: flex;
   align-items: center;
   padding: 0px 10px;
-  background-color: rgba(20,20,20,0.8);
-  box-shadow: 0px 1px 5px 2px rgba(0,0,0,0.8);
+  background-color: rgba(20, 20, 20, 0.8);
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
 `;
 
 const List = styled.ul`
-  display:flex;
+  display: flex;
 `;
 
 const Item = styled.li`
   width: 50px;
   text-align: center;
-  border-bottom: 5px solid ${props => (props.current ? "#3498db" : "transparent")};
+  border-bottom: 5px solid
+    ${(props) => (props.current ? "#3498db" : "transparent")};
   transition: border-bottom 0.5s ease-in-out;
 `;
 
 const SLink = styled(Link)`
   height: 50px;
-  display: block;
+  display: flex;
   align-items: center;
   justify-content: center;
 `;
 
-const Header = ({ location: { pathname }}) => (
-  <Header1>
-  <List>
-    <Item current={pathname === "/"}>
-      <SLink to="/">Movies</SLink>
-    </Item>
-    <Item current={pathname === "/tv"}>
-      <SLink to="/tv">TV</SLink>
-    </Item>
-    <Item current={pathname === "/search"}>
-      <SLink to="/search">Search</SLink>
-    </Item>
-  </List>
-</Header1>
+const Header = ({ location: { pathname } }) => (
+  <HeaderNode>
+    <List>
+      <Item current={pathname === "/"}>
+        <SLink to="/">Movies</SLink>
+      </Item>
+      <Item current={pathname === "/tv"}>
+        <SLink to="/tv">TV</SLink>
+      </Item>
+      <Item current={pathname === "/search"}>
+        <SLink to="/search">Search</SLink>
+      </Item>
+    </List>
+  </HeaderNode>
 );
 
 export default withRouter(Header);
