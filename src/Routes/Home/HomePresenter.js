08a1f776c8de5957d94a067d99ffe2adf92f5d40
김성수 <fereverv@openbase.co.kr>
2021-07-20 23:40:04 +0900

    Section 노드 추가

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,9 +1,21 @@
 import react from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Section from "Components/Section";
 
-const HomePresenter = ({ nowPlaying, upComing, popular, error, loading }) =>
-  null;
+
+const Container = styled.div`
+  padding: 0px 10px;
+`;
+
+const HomePresenter = ({ nowPlaying, upComing, popular, error, loading }) => 
+  loading ? null : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing">{nowPlaying.map(movie => movie.title)}</Section>
+      )}
+    </Container>
+  );
 
 HomePresenter.prototype = {
   nowPlaying: PropTypes.array,
